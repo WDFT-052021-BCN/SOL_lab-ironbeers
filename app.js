46f312d910e5e1f04cf78ef8bf34f6f33f58c7ff
Fede Muniente <73314063+FMP0830@users.noreply.github.com>
2021-06-25 14:39:41 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+// Require packages
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
+
+// To use partials, we have to register them
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
+
+//Setup HBS and views folder
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname + '/views'));
+
+//To use files from public folder
+app.use(express.static('public'));
+
+//Routes
+app.all('/', (req, res) => {
+	res.render('index');
+});
+
+//Route for getting all the beers
+app.all('/beers', (req, res) => {
+	punkAPI
+		.getBeers()
+		.then((responseFromDB) => {
+			console.log('Response is:', responseFromDB);
+
+			res.render('allBeers', { beers: responseFromDB });
+		})
+		.catch((error) => console.log(error));
+});
+
+//Route for getting a random beer
+app.all('/random-beer', (req, res) => {
+	punkAPI
+		.getRandom()
+		.then((responseFromDB) => {
+			console.log('Response is:', responseFromDB);
+
+			const randomBeer = responseFromDB[0];
+			console.log('the beer:', randomBeer);
+
+			res.render('random-beer', randomBeer);
+		})
+		.catch((error) => console.log(error));
+});
+
+app.all('/:id', (req, res) => {
+	const id = req.params.id;
+
+	punkAPI
+		.getBeer(id)
+		.then((responseFromDB) => {
+			console.log('Response is:', responseFromDB);
+
+			const singleBeer = responseFromDB[0];
+			console.log('the beer:', singleBeer);
+
+			res.render('single-beer', singleBeer);
+		})
+		.catch((err) => console.log(err));
+});
+
+app.listen(3000, () => console.log('App is running on port 3000 🎯'));
